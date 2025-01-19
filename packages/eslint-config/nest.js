e9eb901dd46aa2d8e0a7694039cd14e0d35ceb5c
Denys Funderat <funderatdenis@gmail.com>
2025-01-19 16:40:42 +0200

    eslint & prettier configs added

---
+++ packages/eslint-config/nest.js
@@ -0,0 +1,35 @@
+import globals from 'globals';
+import * as eslintPluginNest from 'eslint-plugin-nestjs';
+import baseConfig from '@repo/eslint-config/base';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	...baseConfig,
+	{ ignores: ['dist/*', 'node_modules/*', '.turbo/*', 'coverage/*'] },
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.jest
+			}
+		}
+	},
+	{
+		plugins: {
+			nestjs: eslintPluginNest
+		}
+	},
+	{
+		rules: {
+			'nestjs/use-validation-pipe': 'warn',
+			'prefer-const': 'error',
+			'@typescript-eslint/explicit-member-accessibility': ['error', {
+				accessibility: 'explicit',
+				overrides: {
+					constructors: 'no-public'
+				}
+			}]
+		}
+	}
+];
