6e81b5137d4674c2520245451c8eac1e82f352a1
Denys Funderat <funderatdenis@gmail.com>
2025-01-19 18:33:31 +0200

    husky and lint-staged added

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,37 +1,36 @@
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
-import eslintPluginPrettier from 'eslint-plugin-prettier';
-import prettierOptions from '@repo/prettier-config';
-import tseslintParser from '@typescript-eslint/parser';
-
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import prettierOptions from "@repo/prettier-config";
+import tseslintParser from "@typescript-eslint/parser";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     ignores: [
-      'dist/*',
-      'node_modules/*',
-      '.turbo/*',
-      'coverage/*',
-      '*.config.js',
-      '*.config.mjs',
-      '*.config.mts',
-      '*.config.ts'
-    ]
+      "dist/*",
+      "node_modules/*",
+      ".turbo/*",
+      "coverage/*",
+      "*.config.js",
+      "*.config.mjs",
+      "*.config.mts",
+      "*.config.ts",
+    ],
   },
-  { files: ['**/*.{js,ts}'] },
+  { files: ["**/*.{js,ts}"] },
   {
     languageOptions: {
       parser: tseslintParser,
       parserOptions: {
-        sourceType: 'module',
+        sourceType: "module",
         ecmaFeatures: {
-          modules: true
-        }
-      }
-    }
+          modules: true,
+        },
+      },
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
@@ -39,35 +38,38 @@ export default [
   {
     plugins: {
       turbo: turboPlugin,
-      prettier: eslintPluginPrettier
-    }
+      prettier: eslintPluginPrettier,
+    },
   },
   {
     rules: {
-      'prettier/prettier': ['warn', prettierOptions],
-      quotes: ['error', 'single'],
-      semi: ['error', 'always'],
-      'prefer-const': 'off',
-      'eol-last': ['error', 'always'],
-      'no-multiple-empty-lines': [
-        'error',
+      "prettier/prettier": ["warn", prettierOptions],
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+      "prefer-const": "off",
+      "eol-last": ["error", "always"],
+      "no-multiple-empty-lines": [
+        "error",
         {
           max: 2,
           maxEOF: 0,
-          maxBOF: 0
-        }
+          maxBOF: 0,
+        },
       ],
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-      '@typescript-eslint/no-unused-expressions': ['warn', {
-        allowShortCircuit: true,
-        allowTernary: true
-      }],
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-empty-interface': 'warn',
-    }
-  }
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "@typescript-eslint/no-unused-expressions": [
+        "warn",
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-empty-interface": "warn",
+    },
+  },
 ];
--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -1,34 +1,37 @@
-import globals from 'globals';
-import * as eslintPluginNest from 'eslint-plugin-nestjs';
-import baseConfig from '@repo/eslint-config/base';
+import globals from "globals";
+import * as eslintPluginNest from "eslint-plugin-nestjs";
+import baseConfig from "@repo/eslint-config/base";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	...baseConfig,
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-				...globals.jest
-			}
-		}
-	},
-	{
-		plugins: {
-			nestjs: eslintPluginNest
-		}
-	},
-	{
-		rules: {
-			'nestjs/use-validation-pipe': 'warn',
-			'prefer-const': 'error',
-			'@typescript-eslint/explicit-member-accessibility': ['error', {
-				accessibility: 'explicit',
-				overrides: {
-					constructors: 'no-public'
-				}
-			}]
-		}
-	}
+  ...baseConfig,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  {
+    plugins: {
+      nestjs: eslintPluginNest,
+    },
+  },
+  {
+    rules: {
+      "nestjs/use-validation-pipe": "warn",
+      "prefer-const": "error",
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        {
+          accessibility: "explicit",
+          overrides: {
+            constructors: "no-public",
+          },
+        },
+      ],
+    },
+  },
 ];
