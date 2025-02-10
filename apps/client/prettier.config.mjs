9f2d31b86f0a9a4195e5c0458781eb08a80a0172
Denys Funderat <funderatdenis@gmail.com>
2025-02-10 16:15:22 +0200

    apps/client migrated to nextJS

---
+++ apps/client/prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 90,
+  tabWidth: 2,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  arrowParens: "always",
+  trailingComma: "none",
+  endOfLine: "lf"
+};
