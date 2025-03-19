5e2c1ae8ddbef7f2c70b1e023fbcde74b3c801f3
Denys Funderat <funderatdenis@gmail.com>
2025-03-19 17:06:57 +0200

    add optional port to api-url config

--- apps/client/src/configs/api-url.config.ts
+++ apps/client/src/configs/api-url.config.ts
@@ -1,5 +1,6 @@
 import envConfig from "~/configs/env.config";
 
 const API_BASE_ROUTE = "/api";
+const API_PORT = envConfig.SERVER_PORT ? `:${envConfig.SERVER_PORT}` : "";
 
-export const BASE_URL = `${envConfig.SERVER_PROTOCOL}://${envConfig.SERVER_HOST}${API_BASE_ROUTE}`;
+export const BASE_URL = `${envConfig.SERVER_PROTOCOL}://${envConfig.SERVER_HOST}${API_PORT}${API_BASE_ROUTE}`;
--- apps/client/src/configs/env.config.ts
+++ apps/client/src/configs/env.config.ts
@@ -1,6 +1,7 @@
 const envConfig = {
   SERVER_PROTOCOL: process.env.NEXT_PUBLIC_SERVER_PROTOCOL,
-  SERVER_HOST: process.env.NEXT_PUBLIC_SERVER_HOST
+  SERVER_HOST: process.env.NEXT_PUBLIC_SERVER_HOST,
+  SERVER_PORT: process.env.NEXT_PUBLIC_SERVER_PORT
 };
 
 export default envConfig;
