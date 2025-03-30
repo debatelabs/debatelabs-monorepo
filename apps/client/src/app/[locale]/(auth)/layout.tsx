45f46be21d7f87b98c05a606da9dee3fcfd82e33
Denys Funderat <funderatdenis@gmail.com>
2025-03-30 15:49:43 +0300

    comment removed

--- apps/client/src/app/[locale]/(auth)/layout.tsx
+++ apps/client/src/app/[locale]/(auth)/layout.tsx
@@ -1,21 +1,13 @@
 'use client';
 
-import React, { useLayoutEffect } from 'react';
+import React from 'react';
 import BackButton from '~/core/components/buttons/BackButton';
 import { useTranslation } from 'react-i18next';
 import ROUTES from '~/core/constants/routes';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   const { t } = useTranslation();
-  const router = useRouter();
-
-  useLayoutEffect(() => {
-    // TODO: check if user is logged in
-    //  if isAuthorized === true in redux slice
-    // router.replace(ROUTES.home);
-  }, [router]);
 
   return (
     <div>
