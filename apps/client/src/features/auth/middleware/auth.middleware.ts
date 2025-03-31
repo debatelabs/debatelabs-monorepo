79d3bb02e681b6c809c235470c229bf0c7c91063
Denys Funderat <funderatdenis@gmail.com>
2025-03-31 15:58:27 +0300

    locale excluded from pathname in auth middleware

--- apps/client/src/features/auth/middleware/auth.middleware.ts
+++ apps/client/src/features/auth/middleware/auth.middleware.ts
@@ -5,9 +5,17 @@ import ROUTES, { AUTH_ROUTES, PUBLIC_ROUTES } from '~/core/constants/routes';
 import { checkToken } from '~/infrastructure/services/token.actions';
 import { RedirectThrowable } from '~/core/types/common.types';
 import COOKIES from '~/core/constants/cookies';
+import { i18nConfig } from '~/core/configs/i18n.config';
+
+function getPathname(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
+  const locale = pathname.slice(1, 3) as (typeof i18nConfig.locales)[number];
+  const isLocale = i18nConfig.locales.includes(locale);
+  return isLocale ? pathname.slice(3) : pathname;
+}
 
 export default async function authMiddleware(req: NextRequest, res: NextResponse) {
-  const pathname = req.nextUrl.pathname;
+  const pathname = getPathname(req);
   const isPublicRoute = PUBLIC_ROUTES.includes(pathname);
   const isAuthRoute = AUTH_ROUTES.includes(pathname);
 
